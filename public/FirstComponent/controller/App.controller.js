8595c9b244e0c840ee73be43ea8c4626f117d960
tim-yeowart <tyowie@gmail.com>
2016-10-19 18:49:59 +1000

    added sapui5 stuff

---
+++ public/FirstComponent/controller/App.controller.js
@@ -0,0 +1,19 @@
+﻿sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.wt.controller.App", {
+        
+        onShowHello: function () {
+            // read msg from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
+            MessageToast.show(sMsg);
+        }
+    });
+});
